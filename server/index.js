d1807104af26323dcfac926ce193d8b10cba1556
Austin Westbury <austinwestbury@live.com>
2022-10-24 20:27:22 -0400

    proper push

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51Lv7CLJxmJrVuGJsDzI20YnZk55Jr8TwzwV0CA1dBRDPZAKXyTXVa9HmnaKyR4TLFU3IybzhcSKkiGKQKkGKwrA300A2kDL8tX');
+const app = express();
+const { v4 } = require('uuid');
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send("WORKING")
+})
+
+app.post('/payment', async (req, res) => {
+  try {
+    
+    const { amount } = req.body;
+    
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'cad',
+      payment_method_types: ['card'],
+    });
+
+    res.status(200).json(paymentIntent.client_secret)
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, message: error.message })
+  }
+
+})
+
+app.listen(3030, () => {
+  console.log('server running on 3030')
+})
