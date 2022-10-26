7cf33940894c99839cd5f664b491b28d231aeaf9
Austin Westbury <austinwestbury@live.com>
2022-10-25 23:53:47 -0400

    18

--- server/index.js
+++ server/index.js
@@ -42,7 +42,6 @@ app.post('/api/payment', async (req, res) => {
 if (NODE_ENV === 'production') {
   app.get('/about', (req, res) => {
     console.log('in')
-    let path = req.params['0'].substring(1)
     console.log(path)
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
